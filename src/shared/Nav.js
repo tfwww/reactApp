7a74972c3bc633f2db375207e7079d5812cf7c5f
Winter Zhong <wmzhong@users.noreply.github.com>
2018-04-11 11:23:20 +0800

    导航栏添加跳转功能

--- src/shared/Nav.js
+++ src/shared/Nav.js
@@ -1,48 +1,23 @@
-import React, { Component } from 'react'
-import { Link, Route } from 'react-router-dom'
+/**
+ * 导航栏使用实例
+ * @param {string} navPath 跳转路径
+ * @param {string} navTitle 标题文本
+ * <Nav path='/' title='投资' />
+ * 如果不需跳转功能，则无需 path 属性
+ * <Nav title='投资' />
+ */
 
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './Nav.css'
 
 class Nav extends Component {
-
-    constructor(props) {
-        super(props);
-        // this.state = {inx: this.props.initInx}
-
-        // This binding is necessary to make `this` work in the callback
-        // this.handleClick = this.handleClick.bind(this)
-    }
-
-    // handleClick(inx, event) {
-    //     this.setState({inx: inx})
-    // }
-
-    // 渲染 footer 目录
-    // renderItems() {
-    //     const list = [
-    //         {inx: 0, img: ft1, onImg: ftOn1, content: '首页', link: '/'},
-    //         {inx: 1, img: ft2, onImg: ftOn2, content: '投资', link: '/Invest'},
-    //         {inx: 2, img: ft3, onImg: ftOn3, content: '发现', link: '/Find'},
-    //         {inx: 3, img: ft4, onImg: ftOn4, content: '我的', link: '/Mine'},
-    //     ]
-    //     return list.map((item) =>
-    //         <Link to={item.link} key={item.inx}>
-    //             <li className={this.state.inx === item.inx ? 'on' : ''} onClick={() => this.handleClick(item.inx)}>
-    //                 <h2 className="footer-icon">
-    //                     <img src={this.state.inx === item.inx ? item.onImg : item.img} alt="index" />
-    //                 </h2>
-    //                 <p>{item.content}</p>
-    //             </li>
-    //         </Link>
-    //     )
-    // }
-
     render() {
         return (
             <div className="app-title">
                 {
-                    this.props.back &&
-                    <a className="goBack" href=""></a>
+                    this.props.path &&
+                    <Link className="goBack" to={this.props.path}></Link>
                 }
                 <span>{this.props.title}</span>
             </div>
--- src/views/Invest.js
+++ src/views/Invest.js
@@ -4,12 +4,13 @@ import Footer from '../shared/Footer'
 // import { Switch, Route } from 'react-router-dom'
 import './Invest.css'
 const navTitle = '投资'
+const navPath = '/'
 
 class Invest extends Component {
     render() {
         return (
             <div className="Invest">
-                <Nav back={false} title={navTitle} />
+                <Nav title={navTitle} />
                 <Footer initInx={1} />
             </div>
         );
